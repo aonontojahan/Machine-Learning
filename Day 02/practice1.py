17cf07ee5e7ff1704f2075e17bd6d52b2440d2b0
Aononto Jahan Junnurain <aonontojahan@gmail.com>
2025-08-28 03:31:00 +0600

    Python Basic & Basic Operation

---
+++ Day 02/practice1.py
@@ -0,0 +1,9 @@
+#Taking user input of two number and finding which one is heighest
+
+number1 = int(input("Please enter the first number: "))
+number2 = int(input("Please enter the secound number: "))
+
+if number1>number2:
+    print(f"The heighest number is {number1}")
+else:
+    print(f"The heighest number is {number2}")
