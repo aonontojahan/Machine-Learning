bef945948a861b821733a60e2dbbaaaeca91ec2d
Aononto Jahan Junnurain <aonontojahan@gmail.com>
2025-08-28 04:39:16 +0600

    Add some files

--- Day 02/practice1.py
+++
@@ -1,9 +0,0 @@
-#Taking user input of two number and finding which one is heighest
-
-number1 = int(input("Please enter the first number: "))
-number2 = int(input("Please enter the secound number: "))
-
-if number1>number2:
-    print(f"The heighest number is {number1}")
-else:
-    print(f"The heighest number is {number2}")
